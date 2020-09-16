542a252d9c27592fbfe5deb6bb914e17d1223235
Maciek Kukuczka <maciek@kukuczka.net>
2020-09-16 23:27:39 +0200

    Add workaround to save news

---
+++ POS/Migrations/20200916014539_BloodRel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+namespace POS.Migrations
+{
+
+    public partial class BloodRel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+
+}
