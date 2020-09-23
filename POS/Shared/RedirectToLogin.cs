0868fc55b3938b3cb703c8e1c01dcf8574068c63
Maciek Kukuczka <maciek@kukuczka.net>
2020-09-23 05:00:43 +0200

    Add Splash Screen, authentication

---
+++ POS/Shared/RedirectToLogin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace POS.Shared
+{
+
+    public class RedirectToLoginComponent : ComponentBase
+    {
+        [Inject] protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavigationManager.NavigateTo("/Identity/Account/Login");
+        }
+    }
+
+}
